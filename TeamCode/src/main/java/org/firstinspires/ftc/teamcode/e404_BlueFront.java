85a7f679fa0c76ffd0643a4f2c9b6e9b1f129b41
kalee1 <6152682+kalee1@users.noreply.github.com>
2018-03-05 12:42:59 -0600

    Added javadoc comments from 3.4 repository.
    Copy/pasted javadoc comments from 3.4 repository.  (there were so few comments, that a copy/paste was easy enough.)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/e404_BlueFront.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/e404_BlueFront.java
@@ -4,15 +4,24 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 @Autonomous(name="Blue Front", group="Jewel")
 
+/**
+ * e404_BlueFront extends <code>Error404JewelAutonomous</code> class and has the encoder distances for this quadrant.
+ *
+ * @author Team 8668
+ * @See Error404JewelAutonomous
+ * */
 public class e404_BlueFront extends Error404JewelAutonomous
 
 {
     ///////////////////////////////////////////////////////////////////
 
+    /** The quadrant the robot is in. */
     public e404_BlueFront()
     {
         setLocation("FRONT", "BLUE");
     }
+
+   /** Setting the movement distances for this quadrant. */
     @Override public void init()
     {
         cryptoboxDriveDistance = -780;
@@ -22,10 +31,16 @@ public class e404_BlueFront extends Error404JewelAutonomous
         super.init();  //super.init() method is moved to bottom to not get in the way of the driveStraight() method
 
     }
+
+    /** Overriding the start method to provide a place to put things for when this class is selected. */
     @Override public void start(){
         super.start();
     }
 
+    /**
+     * Reads the pictograph and uses the location to select the correct values for the robot movement.
+     *
+     * @param cryptoboxKey  records the pictograph key as a string */
     @Override protected boolean updateFromVuforia(String cryptoboxKey)
     {
         boolean result = false;
@@ -48,6 +63,7 @@ public class e404_BlueFront extends Error404JewelAutonomous
         return result;
     }
 
+    /** Not used in this class. All of the loop logic is in teh parent class. */
     @Override public void loop ()
     {
         super.loop();
