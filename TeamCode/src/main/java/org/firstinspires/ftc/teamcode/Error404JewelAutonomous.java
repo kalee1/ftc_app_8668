a2d763fc2dc83db51572f7cd43c81574caa52ba0
iamlordvoldemort <32399872+iamlordvoldemort@users.noreply.github.com>
2018-03-06 18:26:16 -0600

    Javadocing the autonomous and the teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Error404JewelAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Error404JewelAutonomous.java
@@ -1,21 +1,36 @@
 package org.firstinspires.ftc.teamcode;
 
+ /**
+ *Error404JewelAutonomous extends <code>Error404_Hardware_Tier2</code> and contains the state
+  * machine that steps through each step in the autonomous mission.
+  *
+  * This year's field presents a unique challenge for autonomous. The field is set up so that there
+  * are four unique starting positions and your robot has to be able to start from any of them. We
+  * could have four individual programs (one for each quadrant) but this is clumsy and opens us up
+  * to bugs. Instead what we did is we made one main autonomous class that holds the framework code
+  * that is the same for all four quadrants and then we have four quadrant-specific child classes
+  * that supply the quadrant-specific movement vaules.
+ *
+ * @author Team 8668
+ * @see Error404_Hardware_Tier2
+ */
 public class Error404JewelAutonomous extends Error404_Hardware_Tier2
 
 {
     //////////////////////////////////////////////////////////////////////////////////
+    /** This int variable sets the starting case value for the state machine (which step in the sequence to start with -- i.e. the first one or case 0) */
     private int state = 0;
     private int encoder=0;
     private double timer=0;
+    /** Initializing the method used for driving from the balancing stone to the cryptobox. */
     protected int cryptoboxDriveDistance;
-    // protected int stoneToMarket;
-    // protected int turnToFrontCryptobox;
-    // protected int turnToRearCryptobox;
+    /** Initializing the method used for turning the correct side of the robot to face the cryptobox. */
     protected int turnToCryptobox;
+    /** Initializing the method used for sliding to align on a particular column of the cryptobox. */
     protected int cryptoboxSlide;
-    // protected int location=0;
-
+    /** A string that indicates which side of the field the robot is on ("blue" or "red") */
     private String fieldSide;
+    /** A string that indicates side location on the field ("front" or "back"). */
     private String sideLocation;
 
     public Error404JewelAutonomous()
@@ -23,6 +38,8 @@ public class Error404JewelAutonomous extends Error404_Hardware_Tier2
     }
 
     //////////////////////////////////////////////////////////////////////////////////
+   /** Gets the gyro heading, sets the jewel servos to the home poition, and grabs the encoder
+    * value of the leftFront drive motor for navigation purposes. */
     @Override public void init(){
         super.init();
         telemetry.addData("Gyro: ", getHeading());
@@ -36,6 +53,8 @@ public class Error404JewelAutonomous extends Error404_Hardware_Tier2
 
 
     //////////////////////////////////////////////////////////////////////////////////
+    /** Determins the field position that robot is starting in and utilizes the coorisponding
+     * child class to get the move values for that quadrant of the field. */
     @Override public void start(){
         super.start();
     }
@@ -52,6 +71,11 @@ public class Error404JewelAutonomous extends Error404_Hardware_Tier2
 
 
     //////////////////////////////////////////////////////////////////////////////////
+    /** Contains the state machine which contains the structure for each move the robot will take
+     * during autonomous. In the cases where the robot will drive somewhere, there are no values
+     * hard-coded in as these values will change depending on what quadrant of the field the robot
+     * is starting in. Instead the Autonomous class sources the quadrant-specific movement values
+     * from field position-dependant child classes. */
     @Override public void loop ()
     {
         switch (state)
@@ -271,20 +295,6 @@ public class Error404JewelAutonomous extends Error404_Hardware_Tier2
         telemetry.addData("7. Cryptobox Slide: ", cryptoboxSlide);
         telemetry.addData("Pattern: ", readCryptograph());
 
-//        switch (location){
-//            case 0:
-//                telemetry.addData("Blue Front","");
-//                break;
-//            case 1:
-//                telemetry.addData("Blue Rear","");
-//                break;
-//            case 2:
-//                telemetry.addData("Red Front","");
-//                break;
-//            case 3:
-//                telemetry.addData("Red Rear","");
-//                break;
-//        }
 
 
 
