14ec2741faaeb3dc195cf8c2491b64e7ab5fb892
Nick B <nickbelbas@earthlink.net>
2018-03-16 09:25:25 -0500

    Added CV versions of autonomous structure and BlueFront
    This should allow us to test the code without changing our existing working code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/e404_BlueFrontCV.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Blue Front CV", group="Jewel")
+
+/**
+ * e404_BlueFront extends <code>Error404JewelAutonomousCV</code> class and has the encoder distances for this quadrant.
+ *
+ * @author Team 8668
+ * @See Error404JewelAutonomous
+ * */
+public class e404_BlueFrontCV extends Error404JewelAutonomousCV
+
+{
+    ///////////////////////////////////////////////////////////////////
+
+    /** The quadrant the robot is in. */
+    public e404_BlueFrontCV()
+    {
+        setLocation("FRONT", "BLUE");
+    }
+
+   /** Setting the movement distances for this quadrant. */
+    @Override public void init()
+    {
+        cryptoboxDriveDistance = -780;
+        cryptoboxSlide=0;
+        turnToCryptobox=80;
+        setMultipleDirections("straight", "reverse");
+        super.init();  //super.init() method is moved to bottom to not get in the way of the driveStraight() method
+
+    }
+
+    /** Overriding the start method to provide a place to put things for when this class is selected. */
+    @Override public void start(){
+        super.start();
+    }
+
+    /**
+     * Reads the pictograph and uses the location to select the correct values for the robot movement.
+     *
+     * @param cryptoboxKey  records the pictograph key as a string */
+    @Override protected boolean updateFromVuforia(String cryptoboxKey)
+    {
+        boolean result = false;
+
+        if(cryptoboxKey.equals("LEFT"))
+        {
+            cryptoboxDriveDistance = -630;
+            result = true;
+        }
+        else if(cryptoboxKey.equals("RIGHT"))
+        {
+            cryptoboxDriveDistance=-990;
+            result = true;
+        }
+        else if(cryptoboxKey.equals("CENTER"))
+        {
+            cryptoboxDriveDistance=-805;
+            result = true;
+        }
+        return result;
+    }
+
+    /** Not used in this class. All of the loop logic is in teh parent class. */
+    @Override public void loop ()
+    {
+        super.loop();
+    } // loop
+
+
+}
